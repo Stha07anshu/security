f7d27a2430044508cebaa2828411a05fcd67fd21
Stha07anshu <ansh091rock@gmail.com>
2025-01-18 18:52:09 +0545

    1

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -29,7 +29,9 @@ const userSchema = new mongoose.Schema({
     otpExpiry: { 
         type: Date, 
         default: null 
-    }
+    },
+    failedAttempts: { type: Number, default: 0 }, // Track failed login attempts
+    lockUntil: { type: Date, default: null }, // Track time when the user can attempt login again
 });
 
 const User = mongoose.model('User', userSchema);
